f177abb0eb5f33ec1fdc4cdd12ce0300a4385289
MaijaCraddock <maijacraddock@gmail.com>
2024-06-23 22:40:27 -0400

    refactor progress

--- CodeMonkeyPrac1/Assets/Scripts/Player.cs
+++ CodeMonkeyPrac1/Assets/Scripts/Player.cs
@@ -5,28 +5,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool isWalking;
     //serialized field opens var to editor without opening it to other scripts
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
     private void Update(){
-        Vector2 inputVector = new Vector2(0,0);
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x += 1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime; //mult by time so time is not depend on framerate
         
-        Debug.Log(inputVector);                
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp (transform.forward, moveDir, Time.deltaTime*rotateSpeed);                
+    }
+
+    public bool IsWalking(){
+        return isWalking;
     }
 }
